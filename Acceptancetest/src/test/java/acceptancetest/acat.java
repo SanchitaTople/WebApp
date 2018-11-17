5e227c5fbfbea13f0a5e270e3be5404b5ed82866
SanchitaTople <SanchitaTople@users.noreply.github.com>
2018-11-17 12:35:22 +0530

    Update acat.java

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -1,4 +1,4 @@
-package acceptancetest;
+package acceptancetest;;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -12,8 +12,8 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://13.58.223.117/ProdWebapp";
-	public String URL1 = "http://13.58.223.117/ProdWebapp/avncreatepage.jsp";
+	public String URL = "http://13.58.223.117:8080/ProdWebapp";
+	public String URL1 = "http://13.58.223.117:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	
